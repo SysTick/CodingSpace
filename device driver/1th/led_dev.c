84d2514dba95989fa350d00a2290b9ba5c546d61
zack.li <zack.li@xbro.cn>
2017-03-15 00:55:15 +0800

    led_bus_drv_dev

---
+++ device driver/1th/led_dev.c
@@ -0,0 +1,36 @@
+/*分配/设置/注册一个platform_device*/
+
+static struct resource led_resource[]{
+	[0] = {
+		.start = 0x56000050,
+		.end   = 0x56000050 + 8 - 1,
+		.flags = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start = 4,
+		.end   = 4,
+		.flags = IORESOURCE_MEM,
+	}
+};
+
+static struct platform_device led_dev = {
+	.name	= "myled",
+	.id     = -1,
+	.num_resources	= ARRAY_SIZE(led_resource),
+	.resource 	= led_resource,
+};
+
+static int led_dev_init(void)
+{
+	platform_device_register(&led_dev);
+	return 0;
+}
+
+static void led_dev_exit(void)
+{
+	platform_device_unregister(&led_dev);
+}
+
+
+
+
